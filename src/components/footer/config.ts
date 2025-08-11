f4fe3ce9c74b8587ee33cf7e16e63ad7cb9c47e8
Anurag-xo <amnrag889@gmail.com>
2025-08-11 18:33:49 +0000

    feat: add header, footer, and preloader

---
+++ src/components/footer/config.ts
@@ -0,0 +1,12 @@
+const footer: { title: string; href: string }[] = [
+  {
+    title: "Blog",
+    href: "https://notrlyanurag.duckdns.org",
+  },
+  {
+    title: "Newsletter",
+    href: "/news",
+  },
+];
+
+export { footer };
